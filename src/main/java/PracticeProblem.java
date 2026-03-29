214203023222ccbabb58d246a94e090fe3929768
Marcotian0211 <350327743@tdsb.ca>
2026-03-29 17:59:42 +0000

    update practice problem3-3

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,50 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static String evenOrOdd(int num){
+		if(num==0){
+			return "Zero";}
+		if (num%2==0){
+        return "Even";
+		}
+		if(num%2!=0){
+			return"Odd";}
+		else{
+			return "";
+		}
+		
+		}
+
+	public static String positiveOrNegative(int num){
+		if(num>0){
+			return "Positive";
+		}
+		if(num<0){
+			return"Negative";
+		}
+       else{
+		return "Zero";
+	   }
+	}
+	public static String pluralize(String word){
+		 word= word.toLowerCase();
+		if(word.endsWith("ey")){
+			return"eys";}
+		if(word.endsWith("y")){
+			return"ies";
+		}
+		
+		if(word.endsWith("ife")){
+			return"ives";
+		}
+		else{
+			return"s";
+		}
+
+	}
+		
+	}
 
 	
 
-}
+
